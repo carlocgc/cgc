d8ccfe4fe445f5fba6b32029f197d01218ea6c02
Carlo Mongiello <carlomongiello@improbable.io>
2021-05-02 21:28:02 +0100

    added weak_ptr

--- cgc/memory.h
+++ cgc/memory.h
@@ -3,53 +3,52 @@
 
 namespace cgc
 {
+	class ref_counter : private non_copyable
+	{
+		int m_count;
+
+	public:
+		ref_counter() : m_count(1) {}
+		ref_counter(ref_counter const&) = delete;
+		ref_counter(ref_counter&&) = delete;
+		~ref_counter() = default;
+
+		int get_count() const { return m_count; }
+		int decrement() { return --m_count; }
+		int increment() { return ++m_count; }
+
+		ref_counter& operator=(ref_counter const&) = delete;
+		ref_counter& operator=(ref_counter&&) = delete;
+	};
+
 	template<typename T>
 	class base_ptr
 	{
 	protected:
-		class ref_counter : private non_copyable
-		{
-			int m_Count;
+		T* m_ptr;
+		ref_counter* m_ref_counter;
 
-		public:
-			ref_counter() : m_Count(1) {}
-			ref_counter(ref_counter const&) = delete;
-			ref_counter(ref_counter&&) = delete;
-			~ref_counter() = default;
-
-			int get_count() const { return m_Count; }
-			int decrement() { return --m_Count; }
-			int increment() { return ++m_Count; }
-
-			explicit operator bool() const { return m_Ptr != nullptr; }
-			ref_counter& operator=(ref_counter const&) = delete;
-			ref_counter& operator=(ref_counter&&) = delete;
-		};
-
-		T* m_Ptr;
-		ref_counter* m_RefCounter;
-
-		base_ptr() : m_Ptr(nullptr), m_RefCounter(nullptr) {}
-		explicit base_ptr(T* ptr) : m_Ptr(ptr), m_RefCounter(new ref_counter()) {}
+		base_ptr() : m_ptr(nullptr), m_ref_counter(nullptr) {}
+		explicit base_ptr(T* ptr) : m_ptr(ptr), m_ref_counter(new ref_counter{}) {}
 		virtual ~base_ptr()
 		{
-			if (m_RefCounter && m_RefCounter->decrement() <= 0)
-			{
-				delete m_RefCounter;
-				delete m_Ptr;
+			if (m_ref_counter && m_ref_counter->decrement() <= 0)
+			{				
+				delete m_ptr;
+				delete m_ref_counter;
 			}
 
-			m_Ptr = nullptr;
-			m_RefCounter = nullptr;
+			m_ptr = nullptr;
+			m_ref_counter = nullptr;
 		}
 
 	public:
 		base_ptr(base_ptr const&) = delete;
 		base_ptr(base_ptr&&) = delete;
 
-		explicit operator bool() const { return m_Ptr != nullptr; }
-		bool operator==(base_ptr const& other) { return m_Ptr == other.m_Ptr; }
-		bool operator!=(base_ptr const& other) { return m_Ptr != other.m_Ptr; }
+		explicit operator bool() const { return m_ptr != nullptr; }
+		bool operator==(base_ptr const& other) { return m_ptr == other.m_ptr; }
+		bool operator!=(base_ptr const& other) { return m_ptr != other.m_ptr; }
 		base_ptr& operator=(base_ptr const&) = delete;
 		base_ptr& operator=(base_ptr&&) = delete;
 	};
@@ -62,62 +61,95 @@ namespace cgc
 		explicit unique_ptr(T* ptr) : base_ptr<T>(ptr) {}
 		unique_ptr(unique_ptr&& other) noexcept : base_ptr<T>()
 		{
-			this->m_Ptr = other.m_Ptr;
-			other.m_Ptr = nullptr;
-			this->m_RefCounter = other.m_RefCounter;
-			other.m_RefCounter = nullptr;
+			this->m_ptr = other.m_ptr;
+			other.m_ptr = nullptr;
+			this->m_ref_counter = other.m_ref_counter;
+			other.m_ref_counter = nullptr;
 		}
 
 		~unique_ptr() override = default;
-		
+
 		unique_ptr& operator=(unique_ptr&& other) noexcept
 		{
-			this->m_Ptr = other.m_Ptr;
-			other.m_Ptr = nullptr;
-			this->m_RefCounter = other.m_RefCounter;
-			other.m_RefCounter = nullptr;
+			this->m_ptr = other.m_ptr;
+			other.m_ptr = nullptr;
+			this->m_ref_counter = other.m_ref_counter;
+			other.m_ref_counter = nullptr;
 			return *this;
 		}
 	};
-	
+
 	template<typename T>
 	class shared_ptr : public base_ptr<T>
 	{
-		
-		
 	public:
+		template<T>
+		class weak_ptr
+		{
+			bool m_expired;
+			shared_ptr<T>* m_parent;
+		
+			explicit weak_ptr(shared_ptr<T> const& ptr) : m_expired(false), m_parent(&ptr) {}
+
+			void set_expired()
+			{
+				m_expired = true;
+				m_parent = nullptr;
+			}
+
+		public:
+			weak_ptr() : m_expired(true), m_parent(nullptr) {}
+			weak_ptr(weak_ptr<T> const& other) : m_expired(other.m_expired), m_parent(other.m_parent) {}
+			weak_ptr(weak_ptr<T>&& other) noexcept : m_expired(other.m_expired), m_parent(other.m_parent) { other.m_expired = true; other.m_parent = nullptr; }
+
+			shared_ptr<T> is_valid()
+			{
+				if (m_expired || m_parent == nullptr)
+				{
+					return shared_ptr<T>();
+				}
+
+				return shared_ptr<T>(*m_parent);
+			}			
+		};
+		
 		shared_ptr() : base_ptr<T>() {}
 		explicit shared_ptr(T* ptr) : base_ptr<T>(ptr) {}
 		shared_ptr(shared_ptr const& other) : base_ptr<T>()
 		{
-			this->m_Ptr = other.m_Ptr;
-			this->m_RefCounter = other.m_RefCounter;
-			this->m_RefCounter->increment();
+			this->m_ptr = other.m_ptr;
+			this->m_ref_counter = other.m_ref_counter;
+			this->m_ref_counter->increment();			
 		}
 		shared_ptr(shared_ptr&& other) noexcept : base_ptr<T>()
 		{
-			this->m_Ptr = other.m_Ptr;
-			other.m_Ptr = nullptr;
-			this->m_RefCounter = other.m_RefCounter;
-			other.m_RefCounter = nullptr;
+			this->m_ptr = other.m_ptr;
+			other.m_ptr = nullptr;
+			this->m_ref_counter = other.m_ref_counter;
+			other.m_ref_counter = nullptr;
 		}
 
 		~shared_ptr() override = default;
 
 		shared_ptr& operator=(shared_ptr const& other)
 		{
-			this->m_Ptr = other.m_Ptr;
-			this->m_RefCounter = other.m_RefCounter;
-			this->m_RefCounter->increment();
+			this->m_ptr = other.m_ptr;
+			this->m_ref_counter = other.m_ref_counter;
+			this->m_ref_counter->increment();
 			return *this;
 		}
 		shared_ptr& operator=(shared_ptr&& other) noexcept
 		{
-			this->m_Ptr = other.m_Ptr;
-			other.m_Ptr = nullptr;
-			this->m_RefCounter = other.m_RefCounter;
-			other.m_RefCounter = nullptr;
+			this->m_ptr = other.m_ptr;
+			other.m_ptr = nullptr;
+			this->m_ref_counter = other.m_ref_counter;
+			other.m_ref_counter = nullptr;
 			return *this;
+		}
+
+		weak_ptr<T> get_weak()
+		{
+			return weak_ptr<T>(&this);
 		}
 	};
 }
