94959edc603c47661edd6d16d43bdafdeb623467
Carlo Mongiello <carlomongiello@improbable.io>
2021-05-03 14:37:22 +0100

    added expire tests for wptr

--- cgc-test/weak_ptr_tests.h
+++ cgc-test/weak_ptr_tests.h
@@ -21,3 +21,38 @@ TEST(WeakPtr, GetWeakNotNull)
 
 	ASSERT_TRUE(wptr.TryLock());
 }
+
+TEST(WeakPtr, WeakPtrDoesExpire)
+{
+	Cgc::WeakPtr<TestStruct> wptr;
+	EXPECT_FALSE(wptr.TryLock());
+
+	{
+		Cgc::SharedPtr<TestStruct> sptr{ new TestStruct{} };
+		wptr = sptr.GetWeak();
+		EXPECT_TRUE(wptr.TryLock());
+	}
+
+	ASSERT_FALSE(wptr.TryLock());
+}
+
+TEST(WeakPtr, AllWeakPtrsDoExpire)
+{
+	const int amount = 10;
+	std::vector < Cgc::WeakPtr<TestStruct>> weak_ptrs;
+
+	{
+		Cgc::SharedPtr<TestStruct> sptr{ new TestStruct{} };
+
+		for (auto i = 0; i < amount; ++i)
+		{
+			weak_ptrs.push_back(sptr.GetWeak());
+			EXPECT_TRUE(weak_ptrs[i].TryLock());
+		}
+	}
+
+	for (auto i = 0; i < amount; ++i)
+	{
+		ASSERT_FALSE(weak_ptrs[i].TryLock());
+	}	
+}
